b099887b23f9ebd9870c34f3795ac23710eceb0a
Jonathan Perry <jpper@umich.edu>
2024-02-04 10:08:58 -0600

    Played around with materials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,40 @@
 import { NextPage } from "next";
 import { Canvas } from "@react-three/fiber";
 // Relative
-import { OrbitControls, Stats } from "@react-three/drei";
+import { OrbitControls, Stats, useTexture } from "@react-three/drei";
+
+const TexturedSpheres = () => {
+  const map = useTexture("./textures/pavement_04_diff_1k.png");
+  const displacementMap = useTexture("./textures/pavement_04_disp_1k.png");
+  const normalMap = useTexture("./textures/pavement_04_nor_gl_1k.png");
+  const roughnessMap = useTexture("./textures/pavement_04_rough_1k.png");
+  return (
+    <>
+      <mesh scale={[0.5, 0.5, 0.5]} position={[-1, 0, 0]}>
+        <sphereGeometry />
+        <meshStandardMaterial map={map} />
+      </mesh>
+      <mesh scale={[0.5, 0.5, 0.5]} position={[0, 0, 0]}>
+        <sphereGeometry />
+        <meshStandardMaterial
+          map={map}
+          normalMap={normalMap}
+          roughnessMap={roughnessMap}
+        />
+      </mesh>
+      <mesh scale={[0.5, 0.5, 0.5]} position={[1, 0, 0]}>
+        <sphereGeometry args={[1, 200, 200]} />
+        <meshStandardMaterial
+          map={map}
+          normalMap={normalMap}
+          roughnessMap={roughnessMap}
+          displacementMap={displacementMap}
+          displacementScale={0.05}
+        />
+      </mesh>
+    </>
+  );
+};
 
 const Home: NextPage = () => {
   const testing = true;
@@ -14,12 +47,9 @@ const Home: NextPage = () => {
         {testing ? <axesHelper args={[2]} /> : null}
         {testing ? <gridHelper args={[10, 10]} /> : null}
         <OrbitControls />
-        <ambientLight intensity={0.1} />
-        <directionalLight position={[0, 0, 5]} />
-        <mesh scale={[0.5, 0.5, 0.5]}>
-          <boxGeometry />
-          <meshStandardMaterial color={"blue"} wireframe />
-        </mesh>
+        <ambientLight intensity={0.3} />
+        <directionalLight position={[0, 5, 5]} />
+        <TexturedSpheres />
       </Canvas>
     </div>
   );
