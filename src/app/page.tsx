0efe9f59f057679668f44e6c192012c9d7970e83
Jonathan Perry <jpper@umich.edu>
2024-02-03 21:58:09 -0600

    Extracted camera/gl to add on orbit controls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { Canvas } from "@react-three/fiber";
 import AnimatedBox from "../../components/AnimatedBox";
+import CameraOrbitController from "../../components/CameraOrbitController";
 
 export default function Home() {
   return (
     <div className="container">
       <Canvas>
+        <CameraOrbitController />
         <ambientLight intensity={0.1} />
         <directionalLight color="red" position={[0, 0, 5]} />
         <AnimatedBox />
